dbbec64efc549830b8b2b19ab3076191bd54cb66
Haritiana Adriano <hei.adriano.4@gmail.com>
2024-02-10 04:13:47 +0300

    chore: init fund migration

---
+++ src/main/java/com/starteo/demo/repository/model/Fund.java
@@ -0,0 +1,25 @@
+package com.starteo.demo.repository.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Fund {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private String id;
+
+  private int value;
+}
---
+++ src/main/resources/db/migration/V0_4__create_fund_table.sql
@@ -0,0 +1,8 @@
+create extension if not exists "uuid-ossp";
+
+create table "fund" (
+    id varchar default uuid_generate_v4(),
+    value integer,
+    user_id varchar constraint user_id_fk references "user"(id),
+    idea_id varchar constraint idea_id_fk references "idea"(id)
+);
