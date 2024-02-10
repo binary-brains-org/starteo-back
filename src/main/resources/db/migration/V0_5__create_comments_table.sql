ae97c4eaa505669c1f4e8ad8270a613b8f237d8e
Cedric <99885260+Cenikl@users.noreply.github.com>
2024-02-10 05:52:00 +0300

    chore: create comment table

---
+++ src/main/resources/db/migration/V0_5__create_comments_table.sql
@@ -0,0 +1,8 @@
+create extension if not exists "uuid-ossp";
+
+create table "comment" (
+    id varchar default uuid_generate_v4(),
+    content TEXT,
+    user_id varchar constraint user_id_fk references "user"(id),
+    idea_id varchar constraint idea_id_fk references "idea"(id)
+);
