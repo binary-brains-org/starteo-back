2a9ebe9230f9ece95e620ec0e32f639fb3f0dbc5
Cedric <99885260+Cenikl@users.noreply.github.com>
2024-02-10 04:04:57 +0300

    chore: idea in database

---
+++ src/main/resources/db/migration/V0_3__create_idea_table.sql
@@ -0,0 +1,12 @@
+create extension if not exists "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS "idea"(
+    id  VARCHAR     CONSTRAINT idea_pk  PRIMARY KEY  DEFAULT uuid_generate_v4(),
+    name    VARCHAR     NOT NULL,
+    status VARCHAR NOT NULL,
+    description VARCHAR NOT NULL,
+    image TEXT,
+    creation_datetime varchar not null,
+    updated_datetime varchar not null,
+    founder varchar constraint user_id_fk references "user"(id)
+);
