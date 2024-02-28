979b364440abd6bcd4e6b3eb9fd790badfc85cc0
Cedric <99885260+Cenikl@users.noreply.github.com>
2024-02-28 14:23:36 +0300

    chore: api and funds controller changed according to reviews

--- src/main/java/com/starteo/demo/endpoint/controller/FundController.java
+++ src/main/java/com/starteo/demo/endpoint/controller/FundController.java
@@ -1,7 +1,6 @@
 package com.starteo.demo.endpoint.controller;
 
 import com.starteo.demo.endpoint.rest.mapper.FundMapper;
-import com.starteo.demo.endpoint.rest.mapper.IdeaMapper;
 import com.starteo.demo.endpoint.rest.model.CreateFund;
 import com.starteo.demo.endpoint.rest.model.Fund;
 import com.starteo.demo.repository.model.User;
@@ -24,24 +23,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class FundController {
   private FundService fundService;
   private FundMapper fundMapper;
-  private IdeaMapper ideaMapper;
   private IdeaService ideaService;
   private AuthService authService;
 
-  @PutMapping("/funds/{idea_id}")
-  public List<Fund> saveFundByIdea(
-      @PathVariable(name = "idea_id") String ideaString,
-      HttpServletRequest request,
-      @RequestBody CreateFund toCreate) {
+  @PutMapping("/funds")
+  public List<Fund> saveFunds(HttpServletRequest request, @RequestBody List<CreateFund> toCreate) {
     User userConnected = authService.whoami(request);
-    com.starteo.demo.repository.model.Fund created =
-        fundService.saveFundByIdeaId(toCreate, ideaString, userConnected);
-    return List.of(fundMapper.toRest(created));
+    List<com.starteo.demo.repository.model.Fund> created =
+        fundService.saveFunds(toCreate, userConnected);
+    return created.stream().map(fundMapper::toRest).toList();
   }
 
-  @GetMapping("/funds/{idea_id}")
-  public List<Fund> getAllFundSOnIdea(
-      @PathVariable("idea_id") String ideaString, HttpServletRequest request) {
+  @GetMapping("/ideas/{idea_id}/funds")
+  public List<Fund> getAllFundSOnIdea(@PathVariable("idea_id") String ideaString) {
     return fundService.getFundsByIdea(ideaService.getById(ideaString)).stream()
         .map(fundMapper::toRest)
         .toList();
--- src/main/java/com/starteo/demo/service/FundService.java
+++ src/main/java/com/starteo/demo/service/FundService.java
@@ -3,31 +3,39 @@ package com.starteo.demo.service;
 import com.starteo.demo.endpoint.rest.model.CreateFund;
 import com.starteo.demo.repository.FundRepository;
 import com.starteo.demo.repository.IdeaRepository;
-import com.starteo.demo.repository.UserRepository;
 import com.starteo.demo.repository.model.Fund;
 import com.starteo.demo.repository.model.Idea;
 import com.starteo.demo.repository.model.User;
+import com.starteo.demo.repository.validator.FundValidator;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
+// TODO : add validator for data integrity
 public class FundService {
   private FundRepository fundRepository;
   private IdeaRepository ideaRepository;
-  private UserRepository userRepository;
+  private FundValidator fundValidator;
 
-  public Fund saveFundByIdeaId(CreateFund createFund, String ideaId, User user) {
-    Idea idea =
-        ideaRepository
-            .findById(ideaId)
-            .orElseThrow(
-                () -> {
-                  throw new RuntimeException("idea not found");
-                });
-    Fund fund = Fund.builder().idea(idea).value(createFund.getValue()).user(user).build();
-    return fundRepository.save(fund);
+  public List<Fund> saveFunds(List<CreateFund> createFund, User user) {
+    List<Fund> funds = new ArrayList<>();
+    fundValidator.accept(createFund);
+    for (CreateFund fund : createFund) {
+      Idea idea =
+          ideaRepository
+              .findById(fund.getIdeaId())
+              .orElseThrow(
+                  () -> {
+                    throw new RuntimeException("idea not found");
+                  });
+      Fund ideaFund = Fund.builder().idea(idea).value(fund.getValue()).user(user).build();
+      fundRepository.save(ideaFund);
+      funds.add(ideaFund);
+    }
+    return funds;
   }
 
   public List<Fund> getFundsByIdea(Idea idea) {
